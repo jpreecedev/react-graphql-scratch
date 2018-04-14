8fe1b6327fe8a5693fe558ad13926172f71eb852
Jon Preece <jonpreece@hotmail.co.uk>
2018-04-14 07:22:45 +0100

    Added users & companies

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: webpack-dev-server --config webpack.dev.js
+api: nodemon ./server/index.js --exec babel-node
+data: json-server ./server/db.json --port=3000
---
+++ server/schema/index.js
@@ -0,0 +1,63 @@
+import { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } from 'graphql'
+import axios from 'axios'
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    },
+    description: {
+      type: GraphQLString
+    }
+  }
+})
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {
+      type: GraphQLString
+    },
+    firstName: {
+      type: GraphQLString
+    },
+    age: {
+      type: GraphQLInt
+    },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(response => response.data)
+      }
+    }
+  }
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data)
+      }
+    }
+  }
+})
+
+export default new GraphQLSchema({
+  query: RootQuery
+})
